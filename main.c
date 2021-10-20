608a0c38e172e1da360f628e62e7bf867c5ab5f9
cc-smith <chriscolinsmith@gmail.com>
2021-10-20 12:14:14 -0600

    Implemented variableExpansion

--- main.c
+++ main.c
@@ -73,21 +73,75 @@ struct userInput* parseInputString(char* inputString) {
     return currInput;
 }
 
+
+
+char* variableExpansion(char* inputString) {
+    // Initialize character pointers 
+    char* ptr1;
+    char* ptr2;
+    ptr1 = inputString;
+    ptr2 = inputString + 1;
+    char* var = "$";
+
+    // Initialize the expanded string
+    char* expandedString;
+    expandedString = malloc(2048 * sizeof(char));
+    memset(expandedString, '\0', 2048);
+
+    // Store the process ID
+    int pid = getpid();
+    char buffer[sizeof(int) * 8 + 1];
+    sprintf(buffer, "%i", pid);
+
+    int i, counter = 0;
+    for (i = 0; i < strlen(inputString); i++) {
+        // Variable $$ found
+        if(*ptr1 == *var && *ptr2 == *var) {
+            ptr1 += 2;
+            ptr2 += 2;
+            strcat(expandedString, buffer); // copy the PID to the expanded string
+            counter += strlen(buffer);  // increment counter by length of PID
+        }
+        // Variable $$ not found, increment pointer and counter by 1
+        else {
+            expandedString[counter] = *ptr1;    // Copy char to expanded string
+            counter++;
+            ptr1++;
+            ptr2++;
+        }
+    }
+ /*   printf("\nbuffer: %s", buffer);
+    printf("\nEXPANDED STRING: %s", expandedString);*/
+    return expandedString;
+}
+
+
+
+
+
 int main(void) {
     struct userInput* newInput;
     char* inputString;
     inputString = malloc(2048 * sizeof(char));
-
     memset(inputString, '\0', 2048);
+
+    // Get input from user
     printf(": ");
     fgets(inputString, 2048, stdin);
-    newInput = parseInputString(inputString);
+
+    // Transform any expansion variables ("$$") in the input
+    char* expandedString = variableExpansion(inputString);
+
+    // Parse the expanded string
+    newInput = parseInputString(expandedString);
+
+
     printf("\nThis is the struct:\n"
         "command: %s\n"
         "args: %s\n"
         "inputFile: %s\n"
         "outputFile: %s\n"
-        "background: %i\n",
+        "background: %i\n\n",
         newInput->command,
         newInput->args,
         newInput->inputFile,
