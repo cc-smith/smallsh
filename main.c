319ca48a05f4e98e15a79c262546bd898c67623b
cc-smith <chriscolinsmith@gmail.com>
2021-10-21 15:46:07 -0600

    Implemented printStruct, refactored parseInputString

--- main.c
+++ main.c
@@ -14,24 +14,26 @@
 struct userInput
 {
     char* command;
-    char* args;
+    char* args[512];
     char* inputFile;
     char* outputFile;
     int background;
 };
 
 struct userInput* parseInputString(char* inputString) {
+
+    // Initialize a struct in which to store the parsed user input
     struct userInput* currInput = malloc(sizeof(struct userInput));
 
+    // Make a copy of the input string for strtok_r calls
     char inputCopy[2048];
     strcpy(inputCopy, inputString);
 
-    // Check if background
+    // Check if command to be executed in the background
     char backgroundFlag[1];
     backgroundFlag[0] = inputCopy[strlen(inputCopy) - 3];
-
     if (strncmp(backgroundFlag, "&", 1) == 0) {
-        currInput->background = 1;
+        currInput->background = 1; // "&" detected, set the background process flag
     }
     else {
         currInput->background = 0;
@@ -45,12 +47,32 @@ struct userInput* parseInputString(char* inputString) {
     currInput->command = calloc(strlen(token) + 1, sizeof(char));
     strcpy(currInput->command, token);
 
+    // Store the command in the list of arguments (for execvp call)
+    currInput->args[0] = malloc(strlen(token) + 1);
+    strcpy(currInput->args[0], token);
+
     // The next token is the arguments
     if (strncmp(saveptr, "<", 1) != 0 && strncmp(saveptr, ">", 1) != 0 && strncmp(saveptr, "&", 1) != 0) {
+        char* saveptr2;
         token = strtok_r(NULL, "<>&\n", &saveptr);
+
+        // Get all the arguments and parse them into a list of lists
         if (token) {
-            currInput->args = calloc(strlen(token) + 1, sizeof(char));
-            strcpy(currInput->args, token);
+            // Store first argument
+            int i = 1;
+            char* token2 = strtok_r(token, " ", &saveptr2);
+            currInput->args[i] = malloc(strlen(token2) + 1);
+            strcpy(currInput->args[i], token2);
+            i++;
+
+            // Store remaining arguments
+            token2 = strtok_r(NULL, " \n", &saveptr2);
+            while (token2 != NULL) {
+                currInput->args[i] = malloc(strlen(token2) + 1);
+                strcpy(currInput->args[i], token2);
+                token2 = strtok_r(NULL, " \n", &saveptr2);
+                i++;
+            }
         }
     }
 
@@ -58,17 +80,19 @@ struct userInput* parseInputString(char* inputString) {
     token = strtok_r(inputCopy, " ", &saveptr);
     while (token != NULL) {
 
+        // Token is an input file
         if (strncmp(token, "<", 1) == 0) {
             token = strtok_r(NULL, " ", &saveptr);
             currInput->inputFile = calloc(strlen(token) + 1, sizeof(char));
             strcpy(currInput->inputFile, token);
         }
-
+        // Token is an output file
         else if (strncmp(token, ">", 1) == 0) {
             token = strtok_r(NULL, " ", &saveptr);
             currInput->outputFile = calloc(strlen(token) + 1, sizeof(char));
             strcpy(currInput->outputFile, token);
         }
+        // Get next token 
         token = strtok_r(NULL, " ", &saveptr);
     }
 
@@ -122,17 +146,22 @@ char* variableExpansion(char* inputString) {
 
 
 void printStruct(struct userInput* input) {
-    printf("\nThis is the struct:\n"
+    printf("\n\n--------------------\nThis is the struct:\n\n"
         "command: %s\n"
-        "args: %s\n"
         "inputFile: %s\n"
         "outputFile: %s\n"
         "background: %i\n",
         input->command,
-        input->args,
         input->inputFile,
         input->outputFile,
         input->background);
+    int i = 0;
+    while (input->args[i] != NULL) {
+            printf("arg%i: %s\n", i, input->args[i]);
+            i++;
+
+        }
+    printf("--------------------\n\n");
     return;
 }
 
@@ -182,9 +211,9 @@ int main(void) {
         }
 
         // Exit from shell
-        if (strcmp(inputString, "exit")) {
+    /*    if (strcmp(inputString, "exit")) {
             return EXIT_SUCCESS;
-        }
+        }*/
 
         // Transform any expansion variables ("$$") in the input
         char* expandedString = variableExpansion(inputString);
@@ -193,11 +222,14 @@ int main(void) {
         parsedInput = parseInputString(expandedString);
         printStruct(parsedInput);
 
-        // Check built in commands (cd, exit, status)
+        // Check if command is a built in command (cd, exit, status)
         if (strcmp(parsedInput->command, "cd") == 0) {
             changeDir(parsedInput->args);
         }
-     
+        
+        // Execute all other commands
+        int execFlag = execCommand(parsedInput);
+        execvp(parsedInput->command, parsedInput->args);
 
     }
     
